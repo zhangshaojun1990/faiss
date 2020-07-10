4f040b68f2c75498f636a5a010ca88fbdbfab163
zhangshaojun1990 <1197940823@qq.com>
2020-07-10 15:25:51 +0800

    Update IndexIVFPQ.h

--- IndexIVFPQ.h
+++ IndexIVFPQ.h
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
+//乘积量化
 // -*- c++ -*-
 
 #ifndef FAISS_INDEX_IVFPQ_H
